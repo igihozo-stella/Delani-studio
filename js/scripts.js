c7a4942d30023db92e95703a966f0814ff72f6cb
igihozo-stella <igihozo96@gmail.com>
2020-10-04 16:27:36 +0200

    add toggle effect on wedo section.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,20 @@ $(document).ready(function(){
         $('.design').slideUp('5000');
         $('.design-text').fadeIn('3000');
     });
+    $('.design-text').click(function(){
+        $('.design').slideDown('5000');
+        $('.design-text').fadeOut('slow');
+
+    });
+
+    $('.developement').click(function(){
+        $('.developement').slideUp('5000');
+        $('.developement-text').fadeIn('3000');
+    });
+
+    $('.developement-text').click(function(){
+        $('.developement').slideDown('5000');
+        $('.developement-text').fadeOut('slow');
+
+    });
+
